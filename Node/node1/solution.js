9f98a5fdd5aeb8cb6cc46c5860ff55bf891a81d2
pelaszlo <pelaszlo@gmail.hu>
2024-11-28 19:29:24 +0100

    feltöltés

---
+++ Node/node1/solution.js
@@ -0,0 +1,21 @@
+// KÖZÖS MEGOLDÁSA A JS ISMÉTLŐ FELADATOKNAK
+
+// 1. Feladat: A legnagyobb elem a tömbben
+
+// 2. Feladat: Számjegyek összege
+
+// 3. Feladat: Termékek kezelése
+
+// 4. Feladat: Számjegyek összege felhasználói input alapján
+
+// 5. Feladat: Páros és páratlan számok különválasztása
+
+// 6. Feladat: Szorzótábla generálása
+
+// 7. Feladat: Faktoriális kiszámítása
+
+// 8. Feladat: Számok összegzése megadott tartományban
+
+// 9. Feladat: Négyzetszámok generálása
+
+// 10. Feladat: Palindróm ellenőrzése
